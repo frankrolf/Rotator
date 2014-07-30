b55d5a044c25f97796cf6b356337521028b25638
Frank Grießhammer <frankrolf@gmail.com>
2014-07-30 19:30:53 +0200

    SuperRotator works!

--- SuperRotator.roboFontExt/lib/SuperRotator.py
+++ SuperRotator.roboFontExt/lib/SuperRotator.py
@@ -1,7 +1,25 @@
 # coding: utf-8
 
-import os
-import plistlib
+'''
+SuperRotator.
+
+Robofont extension for joyful rotation of glyphs and outlines.
+✱ ✲ ✳ ✴ ✵ ✶ ✷ ✸ ✹ ✺ ✻ ✼ ✽ ✾ ✿ ❀ ❁ ❂ ❃ ❄ ❅ ❆ ❇ ❈ ❉ ❊ ❋ 
+Frank Grießhammer – www.frgr.de
+
+Versions:
+0.0 2012        FL version.
+0.1 2013-02-28  Update with plist for storing preferences.
+0.2 2013        Re-write for Robofont.
+0.3 2013-11-08  Add click capture for setting rotation center.
+0.4 2014-07-30  Update UI, get rid of plist, add preview glyph, add rounding possibility for resulting glyph.
+
+
+ToDo:
+- Add NSFormatter to text boxes to only allow numbers for text entry.
+
+'''
+
 
 from vanilla import *
 from AppKit import *
@@ -15,73 +33,34 @@ from fontTools.pens.cocoaPen import CocoaPen
 from lib.fontObjects import internalFontClasses
 from mojo.extensions import getExtensionDefault, setExtensionDefault, getExtensionDefaultColor, setExtensionDefaultColor
 
-rotatorPaletteDefaultKey = "de.frgr.SuperRotator"
-
-def getPrefs():
-    prefPath = os.path.expanduser('~/Library/Preferences/')
-    prefFile = os.sep.join((prefPath, 'de.frgr.SuperRotator.plist'))
-
-
-    'standard plist values'
-    prefDict = {
-        'steps': 6,
-        'x': 0,
-        'y': 0,
-        'capture': False,
-        'round': True,
-    }
-
-    # write standard values in prefs file if it does not exist, or the keys are different.
-    if not os.path.exists(prefFile):
-        plistlib.writePlist(prefDict, prefFile)
-    if sorted(plistlib.readPlist(prefFile).keys()) != sorted(prefDict.keys()):
-        plistlib.writePlist(prefDict, prefFile)
-
-    prefDict.update(plistlib.readPlist(prefFile))
-    return prefDict
-
-
-# def writePrefs(prefDict):
-#     storedPrefs = 
-
-
-# prefDict['pointsize'] = pointsize
-# prefDict['targetLineWidth'] = targetLineWidth
-# prefDict['splitGlyphName'] = splitGlyphName
-# plistlib.writePlist(prefDict, prefFile)
+rotatorDefaults = 'de.frgr.SuperRotator'
 
 
 class SuperRotator(BaseWindowController):
 
     _title = 'SuperRotator'
-    _width = 140
+    _width = 180
     _frame = 8
-    _height = 260
+    _height = 274
     _row = 24
     _padding = 16
     _gutter = 8
     _lineHeight = 20
+    _color = NSColor.colorWithCalibratedRed_green_blue_alpha_(0.0, 0.0, 0.8, .8)
 
-    _columns = 2
+    _columns = 3
     _columnWidth = (_width-((_columns-1)*_gutter))/_columns
     _column_0 = _frame
     _column_1 = _frame + _columnWidth + _gutter
     _column_2 = _frame + 2*_columnWidth + 2*_gutter
     _column_3 = _frame + 3*_columnWidth + 3*_gutter
 
-    # print _column_1,_column_2,_column_3
-    # print _column_0+_columnWidth,_column_1+_columnWidth,_column_2+_columnWidth,_column_3+_columnWidth
-    # _column_2 = _width * 1/2 
-    # _column_3 = _width * 3/4 
-
-    preferences = getPrefs()
-
-    _xValue = preferences['x']
-    _yValue = preferences['y']
-    _steps = preferences['steps']
-    _capture = preferences['capture']
-    _round = preferences['round']
-    _angle = 360.0/_steps
+    xValue = getExtensionDefault('%s.%s' %(rotatorDefaults, 'x'), 0)
+    yValue = getExtensionDefault('%s.%s' %(rotatorDefaults, 'y'), 0)
+    steps = getExtensionDefault('%s.%s' %(rotatorDefaults, 'steps'), 12)
+    capture = getExtensionDefault('%s.%s' %(rotatorDefaults, 'capture'), False)
+    rounding = getExtensionDefault('%s.%s' %(rotatorDefaults, 'round'), False)
+    angle = 360.0/steps
 
 
     def __init__(self):
@@ -89,12 +68,9 @@ class SuperRotator(BaseWindowController):
 
         self.w = FloatingWindow(
                 (self._width+2*self._frame, self._height),
-                self._title, textured=True, closable=True)
+                self._title, textured=True, closable=False)
 
-        # _xValue = int(self.glyph.width/2)
-        self.coords = (self._xValue,self._yValue)
-        # self.activateClickCapture()
-
+        # ----------
         # text boxes
         # ----------
 
@@ -102,199 +78,201 @@ class SuperRotator(BaseWindowController):
 
         self.w.steps_label = TextBox(
                 (self._column_0, textBoxY, self._columnWidth, self._lineHeight),
-                'steps', alignment='right')
-        
+                'Steps', alignment='right')
         self.w.steps_text = EditText(
                 (self._column_1, textBoxY-2, self._columnWidth, self._lineHeight), 
-                self._steps, callback=self.angleCallback, continuous=True)
-
+                self.steps, 
+                callback=self.angleCallback, 
+                continuous=True)
         textBoxY += (self._row)
 
         self.w.xValue_label = TextBox(
                 (self._column_0, textBoxY, self._columnWidth, self._lineHeight),
-                'x', alignment='right')
+                'X', alignment='right')
         self.w.xValue_text = EditText(
                 (self._column_1, textBoxY-2, self._columnWidth, self._lineHeight),
-                self._xValue, callback=self.updateCallback)
-
+                self.xValue, 
+                callback=self.xCallback)
         textBoxY += (self._row)
 
         self.w.yValue_label = TextBox(
                 (self._column_0, textBoxY, self._columnWidth, self._lineHeight),
-                'y', alignment='right')
+                'Y', alignment='right')
         self.w.yValue_text = EditText(
                 (self._column_1, textBoxY-2, self._columnWidth, self._lineHeight),
-                self._yValue, callback=self.updateCallback)
-
+                self.yValue, 
+                callback=self.yCallback)
         textBoxY += (self._row)
 
         self.w.angle_label = TextBox(
                 (self._column_0, textBoxY, self._columnWidth, self._lineHeight),
-                'angle', alignment='right')
+                'Angle', alignment='right')
         self.w.angleResult = TextBox(
                 (self._column_1, textBoxY, self._columnWidth, self._lineHeight),
-                u'%s°' % int(self._angle))
-
+                u'%s°' % self.niceAngleString(self.angle))
         textBoxY += (self._row)
 
-        # self.w.capture_label = TextBox(
-        #         (self._column_0, textBoxY, self._columnWidth, self._lineHeight),
-        #         'Capture clicks', alignment='right')
+        textBoxY += (self._row * .25)
+        self.w.line = HorizontalLine((self._gutter, textBoxY, -self._gutter, 0.5))
+        textBoxY += (self._row * .25)
+
         self.w.capture_checkbox = CheckBox(
-                (self._column_0, textBoxY, self._columnWidth*2, self._lineHeight),
-                'capture clicks', value=self._capture)
-
+                (self._column_1 - 25, textBoxY, -self._gutter, self._lineHeight),
+                'Capture Clicks', 
+                value=self.capture, 
+                callback=self.captureCallback)
         textBoxY += (self._row)
 
-        self.w.round_checkbox = CheckBox(
-                (self._column_0, textBoxY, self._columnWidth*2, self._lineHeight),
-                'round coords', value=self._round)
+        self.w.rounding_checkbox = CheckBox(
+                (self._column_1 - 25, textBoxY, -self._gutter, self._lineHeight),
+                'Round Result', 
+                value=self.rounding, 
+                callback=self.roundingCallback)
+        textBoxY += (self._row)
 
-
-        # self.w.decorationLeft = TextBox(
-        #         (self._column_0, textBoxY, self._columnWidth, 4*self._lineHeight),
-        #         # u'✼ ✢ ✱ ✤\n✤ ✼ ✢ ✱\n✱ ✤ ✼ ✢\n✢ ✱ ✤ ✼', alignment='right')
-        #         u'✼ ✢ ✱\n✤ ✼ ✢\n✱ ✤ ✼\n✢ ✱ ✤', alignment='right')
-
-        # self.w.decorationRight = TextBox(
-        #         (self._column_1, textBoxY, self._columnWidth, 4*self._lineHeight),
-        #         # u'✼ ✢ ✱ ✤\n✤ ✼ ✢ ✱\n✱ ✤ ✼ ✢\n✢ ✱ ✤ ✼', alignment='left')
-        #         u'✢ ✱ ✤\n✼ ✢ ✱\n✤ ✼ ✢\n✱ ✤ ✼', alignment='left')
-
-
+        # -------
         # buttons
         # -------
 
+        self.w.color = ColorWell((self._column_0, textBoxY, -self._gutter, 2*self._lineHeight), 
+                                 color=getExtensionDefaultColor('%s.%s' %(rotatorDefaults, 'color'), self._color),
+                                 callback=self.colorCallback)
+        
+        textBoxY += (self._row)
+
         self.w.buttonClose = Button(
-                (self._column_0, -60, 2*self._columnWidth+self._gutter, 25),
+                (self._column_0, -55, -self._gutter, self._lineHeight),
                 'Close',
                 callback=self.closeCallback)
 
         self.w.buttonRotate = Button(
-                (self._column_0, -30, 2*self._columnWidth+self._gutter, 25),
+                (self._column_0, -30, -self._gutter, self._lineHeight),
                 'Rotate',
                 callback=self.rotateCallback)
 
 
-
         self.setUpBaseWindowBehavior()
-
         addObserver(self, 'mouseUp', 'mouseUp')
-        # addObserver(self, 'modifiersChanged', 'modifiersChanged')
         addObserver(self, 'drawRotationPreview', 'drawBackground')
         self.w.setDefaultButton(self.w.buttonRotate)
-        # self.w.bind('became key', self.activateClickCapture)
         self.w.open()
 
 
-
     def drawRotationPreview(self, info):
-
         outline = self.getRotatedGlyph()
         pen = CocoaPen(None)
+        self.w.color.get().set()
         outline.draw(pen)
-
-        nscolor = NSColor.colorWithCalibratedRed_green_blue_alpha_(0.0, 0.0, 0.8, .8)
-        color = getExtensionDefaultColor("%s.%s" %(rotatorPaletteDefaultKey, "color"), nscolor)
-        # print getExtensionDefaultColor()
         # pen.path.fill()
+        pen.path.setLineWidth_(1)
         pen.path.stroke()
 
 
-
-    # def drawDisplayGlyph(self):
-    #     displayGlyph = RGlyph()
-    #     displayGlyph.width = 0 # self.glyph.width
-    #     displayPen = displayGlyph.getPen()
-    #     coords = (0,0)
-
-
-    #     displayGlyph.scale((3, 3), coords)
-    #     displayGlyph.move((0, 250))
-
-
-    #     return displayGlyph
-
-
-    def mouseUp(self, info):
-        pass
-        # if self.captureClickCoords:
-
-        #     self.coords = int(round(info['point'].x)), int(round(info['point'].y))
-        #     self.w.xValue_text.set(self.coords[0])
-        #     self.w.yValue_text.set(self.coords[1])
-        # else:
-        #     pass
-
-    def updateCallback(self, sender):
+    def xCallback(self, sender):
+        self.xValue = int(sender.get())
         UpdateCurrentGlyphView()
 
 
-    def activateClickCapture(self, sender=None):
-        self.captureClickCoords = True
+    def yCallback(self, sender):
+        self.yValue = int(sender.get())
+        UpdateCurrentGlyphView()
 
 
-    def modifiersChanged(self, info):
-        'Do not capture mouse coordinates when modifier key is down'
-        self.captureClickCoords = not self.captureClickCoords
+    def captureCallback(self, sender):
+        self.capture = not self.capture
+        self.saveDefaults()
+
+
+    def roundingCallback(self, sender):
+        self.rounding = not self.rounding
+        self.saveDefaults()
 
 
     def angleCallback(self, sender):
         stepValue = float(sender.get())
         stepValue = int(round(stepValue))
-        self._steps = stepValue
+        self.steps = stepValue
 
         if abs(stepValue) < 2:
-            self._angle = 90.0
+            self.angle = 90.0
 
         elif stepValue == 0:
-            self._angle = 0
+            self.angle = 0
         
         else:    
-            self._angle = 360/stepValue
+            self.angle = 360/stepValue
 
-        result = int(self._angle) if str(self._angle).endswith('.0') else round(self._angle,2)
-        self._angle = result
-        self.w.angleResult.set(u'%s°' % result)
+        self.w.angleResult.set(u'%s°' % self.niceAngleString(self.angle))
         UpdateCurrentGlyphView()
 
+
+    def niceAngleString(self, angle):
+        angleResultString = u'%.2f' % angle
+        if angleResultString.endswith('.00'):
+            angleResultString = angleResultString[0:-3]
+        return angleResultString
+
+
+
+    def colorCallback(self, sender):
+        setExtensionDefaultColor('%s.%s' %(rotatorDefaults, 'color'), sender.get())
+        UpdateCurrentGlyphView()
+    
 
     def closeCallback(self, sender):
         removeObserver(self, 'mouseUp')
-        # removeObserver(self, 'modifiersChanged')
         removeObserver(self, 'drawBackground')
         UpdateCurrentGlyphView()
+        self.saveDefaults()
         self.w.close()
     
 
     def windowCloseCallback(self, sender):
         removeObserver(self, 'mouseUp')
-        # removeObserver(self, 'modifiersChanged')
         removeObserver(self, 'drawBackground')
         UpdateCurrentGlyphView()
-        # self.w.close()
-        # super(SuperRotator, self).windowCloseCallback(sender)
+        self.saveDefaults()
+
+
+    def mouseUp(self, info):
+        if self.capture:
+            self.xValue, self.yValue = int(round(info['point'].x)), int(round(info['point'].y))
+            self.w.xValue_text.set(self.xValue)
+            self.w.yValue_text.set(self.yValue)
+
+
+    def saveDefaults(self):
+        setExtensionDefault('%s.%s' %(rotatorDefaults, 'x'), self.xValue)
+        setExtensionDefault('%s.%s' %(rotatorDefaults, 'y'), self.yValue)
+        setExtensionDefault('%s.%s' %(rotatorDefaults, 'steps'), self.steps)
+        setExtensionDefault('%s.%s' %(rotatorDefaults, 'capture'), self.capture)
+        setExtensionDefault('%s.%s' %(rotatorDefaults, 'round'), self.rounding)
 
 
     def rotateCallback(self, sender):
-        self.glyph.prepareUndo('Rotate')
+        self.glyph.prepareUndo('SuperRotator')
         rotatedGlyph = self.getRotatedGlyph()
 
         self.glyph.appendGlyph(rotatedGlyph)        
         self.glyph.performUndo()
+        self.saveDefaults()
         self.glyph.update()
 
 
     def getRotatedGlyph(self):
-
         x = int(self.w.xValue_text.get())
         y = int(self.w.yValue_text.get())
 
-        steps = self._steps
-        angle = self._angle
+        steps = self.steps
+        angle = self.angle
+
+        if not any (isinstance(value, (int, float)) for value in [x, y, steps, angle]):
+            print 'nee'
+
         center = (x, y)
-        rGlyph = RGlyph()
-        pen = rGlyph.getPointPen()
+        rotationResult = RGlyph()
+        stepGlyph = RGlyph()
+        pen = stepGlyph.getPointPen()
         contourList = []
         for idx, contour in enumerate(self.glyph):
             if contour.selected:
@@ -306,31 +284,20 @@ class SuperRotator(BaseWindowController):
         
         for contour in contourList:
             self.glyph[contour].drawPoints(pen)
-        
 
-        stepCount = steps - 1
-
-        if steps < 2: 
+        stepCount = steps - 1 # don't draw the original shape again
+        if steps < 2:
             stepCount = 1
             angle = 90
 
-        # tempGlyph = internalFontClasses.createGlyphObject()
-        tempGlyph = RGlyph()
-        tempGlyph.appendGlyph(rGlyph)
+        for i in range(stepCount):
+            stepGlyph.rotate(angle, center)
+            rotationResult.appendGlyph(stepGlyph)
+        
+        if self.rounding:
+            rotationResult.round()
 
-        for i in range(1, stepCount+1):
-            tempGlyph.rotate(angle, center)
-            rGlyph.appendGlyph(tempGlyph)
-            # pen = tempGlyph.getPointPen()
-            # rGlyph.drawPoints(pen)
-            
-        return rGlyph
-
-        # prefDict['pointsize'] = pointsize
-        # prefDict['targetLineWidth'] = targetLineWidth
-        # prefDict['splitGlyphName'] = splitGlyphName
-        # plistlib.writePlist(prefDict, prefFile)
-
+        return rotationResult
 
 
 OpenWindow(SuperRotator)
